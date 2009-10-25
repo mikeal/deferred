855a0bbab367a22eabf14b4cf9b149037ab48c5f
Jonathan Lange <jml@mumak.net>
2009-10-25 13:41:44 +0000

    Move the stuff that's not in defer.py originally to a separate file.

--- deferred/_defer.py
+++ deferred/_defer.py
@@ -14,58 +14,8 @@ import logging
 import traceback
 
 from deferred._failure import Failure
+from deferred._util import mergeFunctionMetadata, unsignedID
 
-
-# modification : unsignedID function from twisted.python.util.unsignedID
-import sys
-_HUGEINT = (sys.maxint + 1L) * 2L
-def unsignedID(obj):
-    """
-    Return the id of an object as an unsigned number so that its hex
-    representation makes sense
-    """
-    rval = id(obj)
-    if rval < 0:
-        rval += _HUGEINT
-    return rval
-
-# modification : mergeFunctionMetadata from twisted.python.util.mergeFunctionMetadata
-import new, inspect
-def mergeFunctionMetadata(f, g):
-    """
-    Overwrite C{g}'s name and docstring with values from C{f}.  Update
-    C{g}'s instance dictionary with C{f}'s.
-
-    To use this function safely you must use the return value. In Python 2.3,
-    L{mergeFunctionMetadata} will create a new function. In later versions of
-    Python, C{g} will be mutated and returned.
-
-    @return: A function that has C{g}'s behavior and metadata merged from
-        C{f}.
-    """
-    try:
-        g.__name__ = f.__name__
-    except TypeError:
-        try:
-            merged = new.function(
-                g.func_code, g.func_globals,
-                f.__name__, inspect.getargspec(g)[-1],
-                g.func_closure)
-        except TypeError:
-            pass
-    else:
-        merged = g
-    try:
-        merged.__doc__ = f.__doc__
-    except (TypeError, AttributeError):
-        pass
-    try:
-        merged.__dict__.update(g.__dict__)
-        merged.__dict__.update(f.__dict__)
-    except (TypeError, AttributeError):
-        pass
-    merged.__module__ = f.__module__
-    return merged
 
 class AlreadyCalledError(Exception):
     pass
