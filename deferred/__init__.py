7294dd31e6944af979c50d54fd99e7bce08441f7
Jonathan Lange <jml@mumak.net>
2009-10-25 13:34:12 +0000

    Add defgen tests.

--- deferred/__init__.py
+++ deferred/__init__.py
@@ -7,5 +7,6 @@ from deferred._defer import DeferredLock, DeferredQueue, DeferredSemaphore
 from deferred._defer import fail, FAILURE, FirstError, gatherResults
 from deferred._defer import getDebugging, maybeDeferred, QueueOverflow
 from deferred._defer import QueueUnderflow, setDebugging, succeed, SUCCESS
+from deferred._defer import deferredGenerator, inlineCallbacks, returnValue, waitForDeferred
 
 from deferred._failure import Failure, NoCurrentExceptionError
